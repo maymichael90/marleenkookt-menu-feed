f1593351aeef3e458565aaa1f472389743ccecdb
maymichael90 <michael@maydium.nl>
2026-03-24 07:38:19 +0700

    Update feed.js

--- api/feed.js
+++ api/feed.js
@@ -103,7 +103,7 @@ module.exports = async (req, res) => {
       price:       p.price,
     }));
 
-    res.status(200).json({ items });
+    res.status(200).json(items);
 
   } catch (err) {
     res.status(500).json({ error: 'Fetch failed', message: err.message });
